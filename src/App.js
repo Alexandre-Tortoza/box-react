2273109f4e4d06d6665b93a5521652ad1187c015
Alexandre Marques <85268093+Alexandre-Tortoza@users.noreply.github.com>
2021-10-15 10:43:31 -0300

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './resert.css'
 import './App.css';
 import Cabecalho from "./components/cabecalho/cabecalho.jsx";
 import Planos from "./components/planos/planos.jsx";
+import Sobre from "./components/sobre/sobre.jsx"
 
 
 
---
+++ src/components/sobre/sobre.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./sobre.css"
+
+class sobre extends Component {
+    render() { 
+        return(
+
+        );
+    }
+}
+
+export default sobre;
